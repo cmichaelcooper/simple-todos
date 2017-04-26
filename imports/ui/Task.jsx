7e5b62e3f6352c6aec00c207957f3e55f065ebb6
Chris Cooper <CCooper@gogoair.com>
2017-04-26 14:04:16 -0500

    add ui components and handling for checking off and filtering tasks by completion

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -18,7 +18,7 @@ export default class Task extends Component {
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const TaskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = this.props.task.checked ? 'checked' : '';
 
     return (
       <li className={taskClassName}>
